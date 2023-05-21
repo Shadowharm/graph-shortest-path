69b79e36d3d3092c9f1e7c72db2f24534988aba6
max.prokhorov <pmv.maks@gmail.com>
2023-05-21 18:03:08 +0300

    feat: add dijkstra algorithm

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const verticesNumbersInput = document.getElementById("vertices");
 const graphInputsBlock = document.getElementById("graph-inputs");
 const mainBlock = document.getElementById("main-block");
 const floydBtn = document.getElementById("floyd-btn");
+const dijkstraBtn = document.getElementById("dijkstra-btn");
 const resultBlock = document.getElementById("result");
 let graphInputs;
 let verticesNumbers = 3;
@@ -92,34 +93,13 @@ verticesNumbersInput.addEventListener("change", (e) => {
   e.target.value = verticesNumbers;
 });
 
-const buildPath = (path, i, j, route) => {
-  if (path[i][j] === i) {
-    return
-  }
-  buildPath(path, i, path[i][j], route);
-  route.push(path[i][j]);
-}
+dijkstraBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 
-floydBtn.addEventListener("click", (e) => {
-  e.preventDefault()
-  const {result, path} = floydAlgorithm(matrix.map(function (item) {
-    return [...item]
-  }), verticesNumbers);
-  //* matrix
-  let str = `<h3>Результат</h3>
-  <div class="d-flex flex-wrap brackets result" style="width: ${verticesNumbers / 12 * 100}%">`
-  for (let i = 0; i < verticesNumbers; i++) {
-    for (let j = 0; j < verticesNumbers; j++) {
-      str += `<div style="flex: 0 0 auto; text-align: center; width: ${
-        (1 / verticesNumbers) * 100
-      }%">${result[i][j] === Infinity ? '∞' : result[i][j]}</div>`;
-    }
-  }
-  str += `</div>`
-
+  let str = `<h3>Результат по методу Дейкстры</h3>`;
 
   //* paths
-  str += `<div class="accordion mt-5">`
+  str += `<div class="accordion mt-5">`;
 
   for (let i = 0; i < verticesNumbers; i++) {
     str += `<div class="accordion-item">
@@ -129,29 +109,94 @@ floydBtn.addEventListener("click", (e) => {
       </button>
     </h2>
     <div id="collapse-${i}" class="accordion-collapse collapse">
-      <div class="accordion-body">`
+      <div class="accordion-body">`;
 
+    const { weight, paths } = dijkstraAlgorithm(verticesNumbers, i, matrix);
 
-      for (let j = 0; j < verticesNumbers; j++) {
-        if (i !== j && path[i][j] === -1) {
-          str += `<p>Пути из вершины ${i + 1} —> ${j + 1} нет`
-        }
-          if (i !== j && path[i][j]!== -1) {
-            const route = [i]
-            buildPath(path, i, j, route)
-            route.push(j)
-            str += `<p>Путь из вершины ${i + 1} —> ${j + 1} имеет длину ${result[i][j]}: ${route.map(number => number + 1).join(" —> ")}</p>`
-          }
-
+    for (let j = 0; j < verticesNumbers; j++) {
+      if (i !== j && weight[j] === Infinity) {
+        str += `<p>Пути из вершины ${i + 1} —> ${j + 1} нет`;
       }
-      str += `</div>
-      </div>
-    </div>`  
-
+      if (i !== j && weight[j] !== Infinity) {
+        str += `<p>Путь из вершины ${i + 1} —> ${j + 1} имеет длину ${
+          weight[j]
+        }: ${paths[j].map((number) => number + 1).join(" —> ")}</p>`;
+      }
     }
+    str += `</div>
+      </div>
+    </div>`;
+  }
 
-  str += `</div>`
+  str += `</div>`;
 
   resultBlock.innerHTML = str;
-    resultBlock.scrollIntoView({ behavior: "smooth"})
+  resultBlock.scrollIntoView({ behavior: "smooth" });
+});
+
+const buildPath = (path, i, j, route) => {
+  if (path[i][j] === i) {
+    return;
+  }
+  buildPath(path, i, path[i][j], route);
+  route.push(path[i][j]);
+};
+
+floydBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const { result, path } = floydAlgorithm(
+    matrix.map(function (item) {
+      return [...item];
+    }),
+    verticesNumbers
+  );
+  //* matrix
+  let str = `<h3>Результат по методу Флойда</h3>
+  <div class="d-flex flex-wrap brackets result" style="width: ${
+    (verticesNumbers / 12) * 100
+  }%">`;
+  for (let i = 0; i < verticesNumbers; i++) {
+    for (let j = 0; j < verticesNumbers; j++) {
+      str += `<div style="flex: 0 0 auto; text-align: center; width: ${
+        (1 / verticesNumbers) * 100
+      }%">${result[i][j] === Infinity ? "∞" : result[i][j]}</div>`;
+    }
+  }
+  str += `</div>`;
+
+  //* paths
+  str += `<div class="accordion mt-5">`;
+
+  for (let i = 0; i < verticesNumbers; i++) {
+    str += `<div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-${i}" aria-expanded="false" aria-controls="collapse-${i}">
+        Пути из вершины ${i + 1}
+      </button>
+    </h2>
+    <div id="collapse-${i}" class="accordion-collapse collapse">
+      <div class="accordion-body">`;
+
+    for (let j = 0; j < verticesNumbers; j++) {
+      if (i !== j && path[i][j] === -1) {
+        str += `<p>Пути из вершины ${i + 1} —> ${j + 1} нет`;
+      }
+      if (i !== j && path[i][j] !== -1) {
+        const route = [i];
+        buildPath(path, i, j, route);
+        route.push(j);
+        str += `<p>Путь из вершины ${i + 1} —> ${j + 1} имеет длину ${
+          result[i][j]
+        }: ${route.map((number) => number + 1).join(" —> ")}</p>`;
+      }
+    }
+    str += `</div>
+      </div>
+    </div>`;
+  }
+
+  str += `</div>`;
+
+  resultBlock.innerHTML = str;
+  resultBlock.scrollIntoView({ behavior: "smooth" });
 });
